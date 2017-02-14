4df989868134d417ae3514b376d668785d20ac04
andreybondar <alanai1995@gmail.com>
2017-02-14 14:39:47 +0300

    Changed instance check to ABC and LoginView

--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 import asyncio
-import collections
+import collections.abc
 from aiohttp import web
 
-from app.exceptions import ValidationError, APIException
+from app.exceptions import APIException
 
 
 class BaseView(web.View):
@@ -13,7 +13,7 @@ class BaseView(web.View):
         except APIException as e:
             if isinstance(e.message, str):
                 message = {'error': e.message}
-            elif isinstance(e.message, collections.Mapping) or isinstance(e.message, collections.Sequence):
+            elif isinstance(e.message, collections.abc.Mapping) or isinstance(e.message, collections.abc.Sequence):
                 message = e.message
             else:
                 message = "Can't serialize {} to dict or list".format(type(e.message).__name__)
@@ -27,12 +27,9 @@ class LoginRequiredView(BaseView):
 
     @asyncio.coroutine
     def __iter__(self):
-        if self.request.method.lower() == 'get':
-            authenticated = yield from self.authentication_class(self.request).authenticate()
-            if not authenticated:
-                return web.json_response({
-                    'error': 'invalid credentials provided'
-                }, status=401)
+        authenticated = yield from self.authentication_class(self.request).authenticate()
+        if not authenticated:
+            return web.json_response({'error': 'invalid credentials provided'}, status=401)
         response = yield from super(LoginRequiredView, self).__iter__()
         return response
 
